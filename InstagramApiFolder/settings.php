ee4e3c1c4f45131c74a2a97742a78d31ad73a08f
Moustapha001 <60150830+Moustapha001@users.noreply.github.com>
2020-05-11 13:31:42 -0400

    Add files via upload

---
+++ InstagramApiFolder/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+
+        define( 'IGAppID', '853404941837918');
+        define( 'IGAppSECRET', 'ba5748aa8ac8acc658d1417e4b06e5ae');
+        define( 'REDIRECT_URI', 'https://www.sartech.us/InstagramApi/InstagramApi.php' );
+        define( 'ACCESS_TOKEN', 'IGQVJXZA091YnFleXpDdTlJaWRZAZAEFjTnR6cDluVmplcDcxQVctenA4aC0yVHhhR2c3VHVjeGFxU0JfNE5IS1BXb2w5cW1qWlc2d2RXSXA4VlpuX0VLYXg1bzRaOURXR3czRFpLcno1aG4wdXJHMVJzeQZDZD');
+ 
+
+ ?>
